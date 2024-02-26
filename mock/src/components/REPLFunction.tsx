7fd1d0042a82c528c566b60590cafd3e25cf94f8
Muhamed Ka <muhamedka@muhamedsair4420.devices.brown.edu>
2024-02-26 17:33:13 -0500

    feat: initial command-REPLFunction map added, REPLFunction class added

---
+++ mock/src/components/REPLFunction.tsx
@@ -0,0 +1,52 @@
+/**
+ * A command-processor function for our REPL. The function returns a string, which is the value to print to history when 
+ * the command is done executing.
+ * 
+ * The arguments passed in the input (which need not be named "args") should 
+ * *NOT* contain the command-name prefix.
+ */
+export interface REPLFunction {    
+    (args: Array<string>): String|String[][]
+}
+
+/**
+ * TODO: Docs for this function.
+ *  
+ * @param
+ * @returns 
+*/
+export function mode (args : string[]) {
+    return args.toString();
+} 
+
+/**
+ * TODO: Docs for this function.
+ *  
+ * @param
+ * @returns
+*/
+export function view (args : string[]) {
+    return args.toString();
+} 
+
+/**
+ * TODO: Docs for this function.
+ *  
+ * @param
+ * @returns 
+*/
+export function load (args : string[]) {
+    return args.toString();
+} 
+
+
+/**
+ * TODO: Docs for this function.
+ *  
+ * @param
+ * @returns 
+*/
+export function search (args : string[]) {
+    return args.toString();
+} 
+
